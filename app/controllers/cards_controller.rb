7cf37ba1f5904dc60ba1a4dfadd4ee96628f366e
Chris Elwood <kudzooman@gmail.com>
2015-07-10 14:32:14 -0400

    made a few changes to views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,6 +8,18 @@ class CardsController < ApplicationController
   end
 
   def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(params.require(:card).permit(:title, :body))
+      if @card.save
+        flash[:notice] = "Bet placed."
+        redirect_to @card
+      else
+        flash[:error] = "There was an error with your bet. Try again."
+        render :new
+      end
   end
 
   def edit
